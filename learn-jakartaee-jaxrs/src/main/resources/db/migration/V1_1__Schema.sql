9714f862d38d65cfead63cada9dd657365f359d5
Michael Puening <mpuening@gmail.com>
2023-05-19 16:18:32 -0400

    Remove database usage from JAX-RS project

--- learn-jakartaee-jaxrs/src/main/resources/db/migration/V1_1__Schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE post(
-	id INT NOT NULL,
-	publish_date DATE NOT NULL,
- 	view_number INT DEFAULT 0,
-	PRIMARY KEY (id)
-);
--- learn-jakartaee-jaxrs/src/main/resources/db/migration/V1_2__Data.sql
+++
@@ -1,1 +0,0 @@
-insert into post(id, publish_date, view_number) values (1, CURRENT DATE, 1);
